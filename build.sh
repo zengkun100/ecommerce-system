b253b879113f9990a70f92ba59c8ef83529e0b2a
wenbo.zhuang <zhuangwen46@163.com>
2024-11-24 00:43:47 +0800

    调整对应端口号

--- build.sh
+++ build.sh
@@ -59,48 +59,7 @@ echo "所有服务启动完成！"
 # 输出服务状态
 docker-compose ps
 
-#docker network rm eureka-service.com zipkin.com config-server.com
-#
-#docker network create eureka-service.com
-#docker network create zipkin.com
-#docker network create config-server.com
-#
-#docker run -d --name zipkin --network zipkin.com -p 9411:9411   openzipkin/zipkin || exit 1
-#
-## 启动 Eureka Server
-#docker run -d --name eureka-server --network eureka-service.com --network zipkin.com --network config-server.com \
-#  -p 8761:8761 eureka-server || exit 1
-#
-## 启动 Config Server
-#docker run -d --name config-server --network eureka-service.com --network zipkin.com --network config-server.com \
-#  -p 8888:8888 config-server || exit 1
-#
-## 启动 API Gateway
-#docker run -d --name gateway-service --network eureka-service.com --network zipkin.com --network config-server.com \
-#  -p 8080:8080 -e EUREKA_SERVER_URL=http://eureka-server:8761/eureka/ \
-#  -e CONFIG_SERVER_URL=http://config-server.com:8888 \
-#  -e ZIPKIN_URL=zipkin.com:5672 \
-#  gateway-service || exit 1
-#
-## 启动 User Service
-#docker run -d --name user-service --network eureka-service.com --network zipkin.com --network config-server.com \
-#  -p 8081:8081  -e EUREKA_SERVER_URL=http://eureka-server:8761/eureka/ \
-#  -e CONFIG_SERVER_URL=http://config-server.com:8888 \
-#  -e ZIPKIN_URL=127.0.0.1:5672 \
-#  user-service
-#
-## 启动 Order Service
-#docker run -d --name order-service --network eureka-service.com --network zipkin.com --network config-server.com \
-#  -p 8082:8082  -e EUREKA_SERVER_URL=http://eureka-server:8761/eureka/ \
-#  -e CONFIG_SERVER_URL=http://config-server.com:8888 \
-#  -e ZIPKIN_URL=zipkin.com:5672 \
-#  order-service
-#
-## 启动 Product Service
-#docker run -d --name product-service --network eureka-service.com --network zipkin.com  --network config-server.com \
-#  -p 8083:8083 -e EUREKA_SERVER_URL=http://eureka-server:8761/eureka/ \
-#  -e CONFIG_SERVER_URL=http://config-server.com:8888 \
-#  -e ZIPKIN_URL=zipkin.com:5672 \
-#  product-service
+#docker exec -it order-service sh
+#apk add curl
 
 echo "All services are running."
